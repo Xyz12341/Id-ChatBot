3acf84812b496abe8b5d994dd64375180b249b99
𝐋𝚄𝙲𝙺𝚈 <166695239+The-LuckyX@users.noreply.github.com>
2024-08-26 05:18:08 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,87 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID", 28178139))
+API_HASH = getenv("API_HASH", "85172511f45230b7f8bb304f5ed8e6d8")
+STRING_SESSION = getenv("STRING_SESSION", None)
+OWNER_ID = int(getenv("OWNER_ID", "7233676524"))
+MONGO_URL = getenv("MONGO_URL", None)
+SUPPORT_GRP = getenv("SUPPORT_GRP", "LuckyXSupport")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "LuckyXUpdate")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "The_LuckyX")
+
+# Random Start Images
+IMG = [
+    "https://telegra.ph/file/725eca518219104e9ff5e.jpg",
+    "https://telegra.ph/file/387ea4170d4c5dd1db722.jpg",
+    "https://telegra.ph/file/99c1167d5f2ed06573583.jpg",
+    "https://telegra.ph/file/a9c0d677b86b51bf550c2.jpg",
+    "https://telegra.ph/file/378e3d663454205c601ce.jpg",
+    "https://telegra.ph/file/0c8c0bb04a3606e84041d.jpg",
+    "https://telegra.ph/file/72bf5b45065a23af56435.jpg",
+    "https://telegra.ph/file/8ac5d43975eba15a2e450.jpg",
+    "https://telegra.ph/file/9003ff277da3335e592b6.jpg",
+    "https://telegra.ph/file/d96e2813d12e1ed80d925.jpg",
+    "https://telegra.ph/file/9499a9d73ded20959ff20.jpg",
+    "https://telegra.ph/file/3aa77e5c0bd286f165bc8.jpg",
+    "https://telegra.ph/file/b0743e4c60ff2cd136bde.jpg",
+    "https://telegra.ph/file/4ce44802b9c4f35b6bcfa.jpg",
+    "https://telegra.ph/file/38eec8196be79908c2ae2.jpg",
+    "https://telegra.ph/file/ae7d618e0288c0ef042e7.jpg",
+    "https://telegra.ph/file/e1eae54c276750c54ee59.jpg",
+    "https://telegra.ph/file/3278fc56ebdbbb4637005.jpg",
+    "https://telegra.ph/file/ae89be758314dad5ddc58.jpg",
+    "https://telegra.ph/file/46963eeae07784b884b4f.jpg",
+    "https://telegra.ph/file/94f91434af52696315207.jpg",
+    "https://telegra.ph/file/4bc7af6da1ecec0bc84d9.jpg",
+    "https://telegra.ph/file/be666b9b79bae213f9414.jpg",
+    "https://telegra.ph/file/3da3f2feadb7525c40cd1.jpg",
+    "https://telegra.ph/file/962c6ee0480b841dead96.jpg",
+    "https://telegra.ph/file/8b443a0cee4382eaaca60.jpg",
+    "https://telegra.ph/file/40ae596873895db8449ae.jpg",
+    "https://telegra.ph/file/f729fbe33a981cf507eaf.jpg",
+    "https://telegra.ph/file/27f3a2f41a2162889543f.jpg",
+    "https://telegra.ph/file/5ec8115aa2f3f6eb462d4.jpg",
+    "https://telegra.ph/file/a22c960d3ae5a93cbd54d.jpg",
+    "https://telegra.ph/file/104437758152e8445cc0a.jpg",
+    "https://telegra.ph/file/ac1628427308c5fc4497b.jpg",
+    "https://telegra.ph/file/18398c28a1a1dbe65f473.jpg",
+    "https://telegra.ph/file/62d1c64fb7467c0f21a0f.jpg",
+    "https://telegra.ph/file/a6d8d7bb1dc849c7ff9ac.jpg",
+    "https://telegra.ph/file/0f75952e36c80df5345ed.jpg",
+    "https://telegra.ph/file/8a654f999f424e4f611fa.jpg",
+    "https://telegra.ph/file/4a2b058865b7f37f3ad3f.jpg",
+    "https://telegra.ph/file/12fa164a5474b5d4ae280.jpg",
+]
+
+
+# Random Stickers
+STICKER = [
+    "CAACAgUAAxkBAAINn2bLva-yg1pTn3rRlBG9w2aXY3boAAIcDwACgPHwV41ME2Yn5oEIHgQ",
+    "CAACAgUAAxkBAAINnmbLvZnFCnUP6O7dfQpwSpLMKkhCAAIJDwACMrTBVw0arZCVtajOHgQ",
+    "CAACAgUAAxkBAAINpmbLvk6lEXxlXsxb_HkXu46LYbVeAALaDAACjTTYV25pJxhBYBLgHgQ",
+    "CAACAgUAAxkBAAINqmbLv2ilN1ZFzMBM7Xcokk28BjZHAAKiDgACFpXgVqBRjO0RDGYvHgQ",
+    "CAACAgUAAxkBAAINqWbLv1xQAS0oeFIboGpKvGsheBj0AAKmDwACc5TZVr3Ou1nDFbfUHgQ",
+    "CAACAgUAAxkBAAINr2bLv6iXrGesUxg9fRQ2P3lSghkXAAJxEQACbF7YVtX-Sb6WWUgJHgQ",
+    "CAACAgUAAxkBAAINsmbLv919AXcM4u2PI1_RUjr6_F0fAAKwEwACmshYVzZtskI0Y-zCHgQ",
+    "CAACAgUAAxkBAAINtWbLwBUz8je354mgdoqS-0VSlOInAAKiDgACFpXgVqBRjO0RDGYvHgQ",
+    "CAACAgUAAxkBAAINuWbLwEI6hOeDfusP9bbYiHlgzhRoAAIdDwACGALhVrRC0cJaWQLEHgQ",
+    "CAACAgUAAxkBAAINvGbLwHvO9WuKgKr4eIe2Dx-5zFHpAAKUDwAC_dTZVh7Btp3FCqCVHgQ",
+]
+
+
+EMOJIOS = [
+    "🥵",
+    "💋",
+    "🫦",
+    "💦",
+    "💘",
+    "😅",
+    "😍",
+    "💖",
+    "🤑",
+    "✨",
+]
